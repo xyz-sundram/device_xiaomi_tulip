8f8ec86164ccfb40c33e3ff07a0e371050b248a3
muti605 <mustafa.sahin605@yandex.com>
2021-07-14 15:21:36 +0300

    tulip: twolip -> tulip

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,10 +25,10 @@
 # Inherit from sdm660-common
 include device/xiaomi/sdm660-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/xiaomi/twolip
+DEVICE_PATH := device/xiaomi/tulip
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := tulip,twolip
+TARGET_OTA_ASSERT_DEVICE := tulip
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
@@ -53,4 +53,4 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 VENDOR_SECURITY_PATCH := 2018-11-01
 
 # Inherit the proprietary files
-include vendor/xiaomi/twolip/BoardConfigVendor.mk
+include vendor/xiaomi/tulip/BoardConfigVendor.mk
