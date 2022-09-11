a621f0fbf6b97369c200a5b0da21bfd377c124b4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-12 00:06:27 +0200

    twolip: Use SDM636 powerhint config
    Change-Id: Ie1b7d9f735b7842bb0cba61c0bde71baae089b44

--- device.mk
+++ device.mk
@@ -338,8 +338,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/powerhint_636.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint_636.json \
-    $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(DEVICE_PATH)/configs/powerhint_636.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint_636.json
 
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
