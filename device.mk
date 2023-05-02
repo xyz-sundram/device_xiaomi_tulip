22eac5388a1f86eeaa12bad161133f2d89991ff2
Edwin Wong <edwinwong@google.com>
2023-05-02 16:43:48 +0300

    twolip: Update clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I406b7d3c2921d4698535421894c6eb3785eb1c0d
    Signed-off-by: mutixd <afatsum.605@gmail.com>

--- device.mk
+++ device.mk
@@ -197,7 +197,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # FM
 BOARD_HAVE_QCOM_FM := true
