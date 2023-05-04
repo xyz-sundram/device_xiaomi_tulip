9199090fdc1e08de32e3406773af7e8bf3525f76
Bruno Martins <bgcngm@gmail.com>
2023-05-04 15:28:46 +0300

    twolip: Migrate to AIDL ClearKey DRM HAL
    Signed-off-by: mutixd <afatsum.605@gmail.com>

--- device.mk
+++ device.mk
@@ -189,7 +189,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # FM
 BOARD_HAVE_QCOM_FM := true
