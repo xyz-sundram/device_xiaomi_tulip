7e0f4225ee40706faf607c9eb2fbd3bd1d7ea866
Pavel Dubrova <pashadubrova@gmail.com>
2023-05-04 15:55:18 +0300

    twolip: Provide android.frameworks.sensorservice@1.0 on vendor
    Starting with Android 12 most HAL libraries are not included
    in VNDK anymore - they need to be explicitly pulled
    in to  for odm blobs to be allowed to access them:
    
        QCamera : <MCI><ERROR> mm_camera_load_shim_lib: 3306: dlopen failed
        with error dlopen failed: library
        android.frameworks.sensorservice@1.0.so not found: needed by
        /odm/lib/libmmcamera2_stats_modules.so in namespace (default)
    
    android.frameworks.sensorservice@1.0 is used by at least mm-camera
    framework.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>
    Change-Id: I72d9bcda1fdfd48c5fb9cb21666edd995ae4d9af
    Signed-off-by: mutixd <afatsum.605@gmail.com>

--- device.mk
+++ device.mk
@@ -376,7 +376,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    android.frameworks.sensorservice@1.0.vendor
 	
 # Shims
 PRODUCT_PACKAGES += \
