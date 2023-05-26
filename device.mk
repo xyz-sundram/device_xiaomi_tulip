42d7c6381fdc98bc132f9acdfe8b5a9045c64bf3
Park Ju Hyung <qkrwngud825@gmail.com>
2023-05-26 15:03:36 +0300

    twolip: Optimize everything on preopt
    We have plenty of space left at /system
    
    Change-Id: I39d552259b66ec329c56cd4f4a7fc46fb2c07457
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: orgesified <gesishabani1@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: mutixd <afatsum.605@gmail.com>

--- device.mk
+++ device.mk
@@ -391,7 +391,7 @@ PRODUCT_PACKAGES += \
     libpiex_shim  
 
 # Speed profile services and wifi-service to reduce RAM and storage.
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := everything
 
 # Always preopt extracted APKs to prevent extracting out of the APK for gms
 # modules.
