64333d4c5d648315207520d1e183313a5547361e
sundram <sundrraam@gmail.com>
2023-06-21 07:27:53 +0530

    tulip:lineage bringup
    ayo wtf gone wrong with last comiit?

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_tulip.mk
+    $(LOCAL_DIR)/lineage_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_tulip-user \
-    aosp_tulip-userdebug \
-    aosp_tulip-eng 
+    lineage_tulip-user \
+    lineage_tulip-userdebug \
+    lineage_tulip-eng 
