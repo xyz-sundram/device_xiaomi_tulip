c3672dac3061042d073e29045b75006764aed11a
userariii <abhattacharjee717@gmail.com>
2023-06-24 17:51:02 +0000

    twolip: Import speed profile services

--- device.mk
+++ device.mk
@@ -399,6 +399,8 @@ PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
 # Use a profile based boot image for this device. Note that this is currently a
 # generic profile and not Android Go optimized.
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
