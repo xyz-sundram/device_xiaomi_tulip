11f387c1acaacff16a5af2d1445fadae96da6a04
sundram <sundrraam@gmail.com>
2023-07-19 23:00:15 +0530

    tulip:inherit gapps

--- lineage_tulip.mk
+++ lineage_tulip.mk
@@ -21,6 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/gapps/common/common-vendor.mk)
 
 # Inherit from tulip device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
